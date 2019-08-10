9fc8357109abad1dcdbade983b8944cea3aed694
syncatallah <ahmed.atallah@swvl.com>
2019-08-10 18:03:46 +0200

    add card lazy loading

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,11 @@
 /** @jsx jsx */
 import jsx from "../jsx";
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import theme from "../theme";
 import Container from "../components/Container";
 import { H1, H2, Section, H3, Div } from "../elements";
 import Flex from "../components/Flex";
 import Header from "../containers/Header";
-import Card from "../components/Card";
 import Connect from "../containers/Connect";
 import { ThemeProvider, ColorMode } from "theme-ui";
 import Footer from "../containers/Footer";
@@ -14,6 +13,8 @@ import nix from "../assets/images/nixverse-without-labels.png";
 import frames from "../assets/images/frames.jpg";
 import Quotation from "../containers/Qoutation";
 import music from "../assets/images/music.jpg";
+
+const Card = lazy(() => import("../components/Card"));
 
 function Home() {
   return (
@@ -46,69 +47,76 @@ function Home() {
         <Connect />
       </Section>
       <Flex minWidth={["1884px"]} justifyContent="center" flexWrap="wrap">
-        <Card
-          title="Mapping a universe of open source software"
-          subLink="https://www.tweag.io/posts/2019-02-06-mapping-open-source.html"
-          image={nix}
-          tag="Readings"
-          link={{
-            text: "See more",
-            href: "/readings"
-          }}
-          maxWidth="25em"
-          mx={[2, 3, 4]}
-          mb={[2, 2, 0]}
-          borderColor="currencolor"
-          width={[1, 1 / 2, 1 / 4]}
-        />
-
-        <Card
-          title="Coming Soon"
-          text=""
-          tag="Writings"
-          link={{
-            text: "See more",
-            href: "/writings"
-          }}
-          maxWidth="25em"
-          mx={[2, 3, 4]}
-          mb={2}
-          borderColor="currencolor"
-          width={[1, 1 / 2, 1 / 4]}
-        >
-          <Quotation />
-        </Card>
-        <Card
-          title="I want to be the scenery"
-          subtitle="Cinema . Phone Camera"
-          text=""
-          image={frames}
-          tag="Frames"
-          link={{
-            text: "See more",
-            href: "/Frames"
-          }}
-          maxWidth="25em"
-          mx={[2, 3, 4]}
-          mb={2}
-          borderColor="currencolor"
-          width={[1, 1 / 2, 1 / 4]}
-        />
-        <Card
-          title="A music that make me quite"
-          subtitle="Indie . Soundtracks . Rock .etc."
-          image={music}
-          tag="Music"
-          link={{
-            text: "See more",
-            href: "/music"
-          }}
-          maxWidth="25em"
-          mx={[2, 3, 4]}
-          mb={[2, 2, 0]}
-          borderColor="currencolor"
-          width={[1, 1 / 2, 1 / 4]}
-        />
+        <Suspense fallback={null}>
+          <Card
+            title="Mapping a universe of open source software"
+            subLink="https://www.tweag.io/posts/2019-02-06-mapping-open-source.html"
+            image={nix}
+            tag="Readings"
+            link={{
+              text: "See more",
+              href: "/readings"
+            }}
+            maxWidth="25em"
+            mx={[2, 3, 4]}
+            mb={[2, 2, 0]}
+            borderColor="currencolor"
+            width={[1, 1 / 2, 1 / 4]}
+          />
+        </Suspense>
+        <Suspense fallback={null}>
+          <Card
+            title="Coming Soon"
+            text=""
+            tag="Writings"
+            link={{
+              text: "See more",
+              href: "/writings"
+            }}
+            maxWidth="25em"
+            mx={[2, 3, 4]}
+            mb={2}
+            borderColor="currencolor"
+            width={[1, 1 / 2, 1 / 4]}
+          >
+            <Quotation />
+          </Card>
+        </Suspense>
+        <Suspense fallback={null}>
+          <Card
+            title="I want to be the scenery"
+            subtitle="Cinema . Phone Camera"
+            text=""
+            image={frames}
+            tag="Frames"
+            link={{
+              text: "See more",
+              href: "/Frames"
+            }}
+            maxWidth="25em"
+            mx={[2, 3, 4]}
+            mb={2}
+            borderColor="currencolor"
+            width={[1, 1 / 2, 1 / 4]}
+          />
+        </Suspense>
+        <Suspense fallback={null}>
+          <Card
+            title="A music that make me quite"
+            subtitle="Indie . Soundtracks . Rock .etc."
+            image={music}
+            tag="Music"
+            link={{
+              text: "See more",
+              href: "/music"
+            }}
+            maxWidth="25em"
+            mx={[2, 3, 4]}
+            mb={[2, 2, 0]}
+            borderColor="currencolor"
+            width={[1, 1 / 2, 1 / 4]}
+          />
+        </Suspense>
       </Flex>
       <Footer />
     </ThemeProvider>
