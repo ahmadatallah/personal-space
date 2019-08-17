1229be6e4adc5da555c15d01a21798071e458091
syncatallah <ahmed.atallah@swvl.com>
2019-08-17 19:29:35 +0200

    add link-icon in readings

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -3,13 +3,15 @@ import jsx from "../jsx";
 import React from "react";
 import theme from "../theme";
 import Flex from "../components/Flex";
-import { P, A } from "../elements";
+import { P } from "../elements";
+import BlockLinkGrow from '../components/BlockLinkGrow.js';
+
 function Footer() {
   return (
     <footer>
       <Flex justifyContent="center" my={0}>
         <P fontFamily={theme.typefaces.mono}>© 2019 syncatallah. </P>
-        <A
+        <BlockLinkGrow
           ml={3}
           fontWeight={700}
           color="currentcolor"
@@ -18,7 +20,7 @@ function Footer() {
           fontFamily={theme.typefaces.mono}
         >
           {"About"}
-        </A>
+        </BlockLinkGrow>
       </Flex>
     </footer>
   );
--- src/containers/Readings.js
+++ src/containers/Readings.js
@@ -2,42 +2,64 @@
 import jsx from "../jsx";
 import React from "react";
 import theme from "../theme";
-import { H1 } from "../elements";
+import { H1, Div } from "../elements";
 import Link from "../components/Link";
+import BlockLinkGrow from "../components/BlockLinkGrow";
+import { IoIosLink } from "react-icons/io";
+
+const LinkIcon = ({ href }) => {
+  return (
+    <BlockLinkGrow href={href}>
+      <IoIosLink size={24} />
+    </BlockLinkGrow>
+  );
+};
 
 function Readings({ title, href, ...props }) {
   return (
     <>
       <H1 fontWeight={400}> Readings </H1>
-      <Link
-        title="No, I have no side code projects to show you"
-        href="https://bit.ly/33zdmtc"
-        fontFamily={theme.typefaces.mono}
-      >
-        {" "}
-      </Link>
-      <Link
-        title="Mapping a universe of open source software"
-        href="https://www.tweag.io/posts/2019-02-06-mapping-open-source.html"
-        fontFamily={theme.typefaces.mono}
-      >
-        {" "}
-      </Link>
-      <Link
-        title="React as a UI runtime"
-        href="https://overreacted.io/react-as-a-ui-runtime/"
-        fontFamily={theme.typefaces.mono}
-      >
-        {" "}
-      </Link>
+      <Div mb={2}>
+        <LinkIcon href="https://bit.ly/33zdmtc" />
+        <Link
+          title="No, I have no side code projects to show you"
+          href="https://bit.ly/33zdmtc"
+          fontFamily={theme.typefaces.mono}
+        >
+          {" "}
+        </Link>
+      </Div>
+      <Div mb={2}>
+        <LinkIcon href="https://www.tweag.io/posts/2019-02-06-mapping-open-source.html" />
 
-      <Link
-        title="Category Theory for Programmers: The Preface"
-        href="https://bartoszmilewski.com/2014/10/28/category-theory-for-programmers-the-preface/"
-        fontFamily={theme.typefaces.mono}
-      >
-        {" "}
-      </Link>
+        <Link
+          title="Mapping a universe of open source software"
+          href="https://www.tweag.io/posts/2019-02-06-mapping-open-source.html"
+          fontFamily={theme.typefaces.mono}
+        >
+          {" "}
+        </Link>
+      </Div>
+      <Div mb={2}>
+        <LinkIcon href="https://overreacted.io/react-as-a-ui-runtime/" />
+        <Link
+          title="React as a UI runtime"
+          href="https://overreacted.io/react-as-a-ui-runtime/"
+          fontFamily={theme.typefaces.mono}
+        >
+          {" "}
+        </Link>
+      </Div>
+      <Div mb={2}>
+        <LinkIcon href="https://bartoszmilewski.com/2014/10/28/category-theory-for-programmers-the-preface/" />
+        <Link
+          title="Category Theory for Programmers: The Preface"
+          href="https://bartoszmilewski.com/2014/10/28/category-theory-for-programmers-the-preface/"
+          fontFamily={theme.typefaces.mono}
+        >
+          {" "}
+        </Link>
+      </Div>
     </>
   );
 }
