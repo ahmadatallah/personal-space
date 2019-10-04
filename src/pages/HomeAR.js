6b3751341812a31cd391dc1640f2115c7a587d1f
syncatallah <ahmed.atallah@swvl.com>
2019-10-04 23:39:25 +0200

    update fonts and link styles

--- src/pages/HomeAR.js
+++ src/pages/HomeAR.js
@@ -3,13 +3,13 @@ import jsx from "../jsx";
 import React, { useState, useEffect } from "react";
 import Link from "../components/Link";
 import theme from "../theme";
-import { H1, Div } from "../elements";
+import { H1, Div, Small } from "../elements";
 import Header from "../containers/Header";
-import { Link as L } from "react-router-dom";
 import Flex from "../components/Flex";
 import { modes } from "../utils/constants";
 import { useColorMode } from "theme-ui";
 import { IoMdQuote } from "react-icons/io";
+import StyledLink from "../components/StyledLink";
 
 const LinkIcon = ({ color }) => {
   return <IoMdQuote size={18} color={color} />;
@@ -45,80 +45,80 @@ function HomeAR({ ...props }) {
         minHeight="100%"
         mr="auto"
         ml="auto"
+        fontFamily={theme.typefaces.helvetica}
+        dir="rtl"
       >
-        <Div dir="rtl" display="grid" pr={[5, 6, 6]} py={5} mt={70}>
-          <H1 fontWeight={400} color={textColor}>
+        <Div display="grid" pr={[5, 6, 6]} py={5} mt={70}>
+          <H1 fontWeight={600} color={textColor}>
             {" "}
             كتابات{" "}
           </H1>
-          <Div mb={3}>
+          <Div mb={2}>
             <LinkIcon color={textColor} />
-            <L
-              to="/ar/school-of-biomedical-engineering"
-              style={{
-                textDecoration: "none",
-                color: "currentcolor",
-                marginBottom: "16px"
-              }}
-            >
+            <StyledLink to="/ar/school-of-biomedical-engineering">
               <Link
                 title="كلية الهندسة الحيوية والطبية والمنظومات ١"
-                fontFamily={theme.typefaces.helvetica}
+                fontWeight={600}
               />
-            </L>
+            </StyledLink>
             <LinkIcon color={textColor} />
+            <Div mr={3}>
+              <Small>
+                ١٢ اغسطس ٢٠١٩{" "}
+                <span role="img" aria-label="min-read">
+                  • ☕️☕️️️️️{" "}
+                </span>
+              </Small>
+            </Div>
           </Div>
-          <Div mb={3}>
+          <Div mb={2}>
             <LinkIcon color={textColor} />
-            <L
-              to="/ar/sorry-being-born-rich-still-leads-to-success"
-              style={{
-                textDecoration: "none",
-                color: "currentcolor",
-                marginBottom: "16px"
-              }}
-            >
+            <StyledLink to="/ar/sorry-being-born-rich-still-leads-to-success">
               <Link
                 title="الملعقة الذهب التي تؤدي دائمًا للنجاح"
-                fontFamily={theme.typefaces.helvetica}
+                fontWeight={600}
               />
-            </L>
+            </StyledLink>
             <LinkIcon color={textColor} />
+            <Div mr={2}>
+              <Small>
+                ١٣ اغسطس ٢٠١٩{" "}
+                <span role="img" aria-label="min-read">
+                  • ☕️{" "}
+                </span>
+              </Small>
+            </Div>
           </Div>
-          <Div mb={3}>
+          <Div mb={2}>
             <LinkIcon color={textColor} />
-            <L
-              to="/ar/happy-society"
-              style={{
-                textDecoration: "none",
-                color: "currentcolor",
-                marginBottom: "16px"
-              }}
-            >
-              <Link
-                title="المجتمعات السعيدة"
-                fontFamily={theme.typefaces.helvetica}
-              />
-            </L>
+            <StyledLink to="/ar/happy-society">
+              <Link title="المجتمعات السعيدة" fontWeight={600} />
+            </StyledLink>
             <LinkIcon color={textColor} />
+            <Div mr={3}>
+              <Small>
+                ١٧ اغسطس ٢٠١٩{" "}
+                <span role="img" aria-label="min-read">
+                  • ☕️{" "}
+                </span>
+              </Small>
+            </Div>
           </Div>
           <Div mb={3}>
             <LinkIcon color={textColor} />
 
-            <L
-              to="/ar/midnight-story"
-              style={{
-                textDecoration: "none",
-                color: "currentcolor",
-                marginBottom: "16px"
-              }}
-            >
-              <Link
-                title="قصة في منتصف الليل"
-                fontFamily={theme.typefaces.helvetica}
-              />
-            </L>
+            <StyledLink to="/ar/midnight-story">
+              <Link title="قصة في منتصف الليل" fontWeight={600} />
+            </StyledLink>
             <LinkIcon color={textColor} />
+            <Div mr={3}>
+              <Small>
+                ١ سبتمبر ٢٠١٩{" "}
+                <span role="img" aria-label="min-read">
+                  • ☕️{" "}
+                </span>
+              </Small>
+            </Div>
           </Div>
         </Div>
       </Flex>
