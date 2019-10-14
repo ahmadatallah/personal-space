a664ebdaefc7b497860b6c9498f383df2accf979
Ahmad Atallah <ahmed.atallah@swvl.com>
2019-10-14 03:07:44 +0200

    router enhancement (#9)
    * migrate header navigation to react router
    
    * fix Frames spaces
    
    * fix page initialisation
    
    * remove lazy load the pages

--- src/App.js
+++ src/App.js
@@ -2,9 +2,19 @@ import React, { lazy, Suspense } from "react";
 import { ThemeProvider, ColorMode } from "theme-ui";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import theme from "./theme";
+import { H2 } from "./elements";
 import GlobalStyles from "./global-styles";
+import Loading from "./components/Loading";
+import Flex from "./components/Flex";
 
-// import pages here
+import About from "./pages/About";
+import Frames from "./pages/Frames";
+import Home from "./pages/Home";
+import HomeAR from "./pages/HomeAR";
+import NotFound from "./pages/NotFound";
+import Readings from "./pages/Readings";
+import Writings from "./pages/Writings";
+
 import Div from "./elements/Div";
 
 const Bio = lazy(() => import("./pages/ar/Bio"));
@@ -18,14 +28,6 @@ const PascalTypeScript = lazy(() =>
 );
 const Society = lazy(() => import("./pages/ar/Society"));
 
-const About = lazy(() => import("./pages/About"));
-const Frames = lazy(() => import("./pages/Frames"));
-const Home = lazy(() => import("./pages/Home"));
-const HomeAR = lazy(() => import("./pages/HomeAR"));
-const NotFound = lazy(() => import("./pages/NotFound"));
-const Readings = lazy(() => import("./pages/Readings"));
-const Writings = lazy(() => import("./pages/Writings"));
-
 function App() {
   return (
     <ThemeProvider theme={theme}>
@@ -34,24 +36,8 @@ function App() {
       <Div fontFamily={theme.typefaces.sansSerif}>
         <Router>
           <Switch>
-            <Route
-              exact
-              path="/"
-              component={() => (
-                <Suspense fallback={null}>
-                  <Home />
-                </Suspense>
-              )}
-            />
-            <Route
-              exact
-              path="/ar"
-              component={() => (
-                <Suspense fallback={null}>
-                  <HomeAR />
-                </Suspense>
-              )}
-            />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/ar" component={HomeAR} />
             <Route
               path={`/ar/school-of-biomedical-engineering`}
               component={() => (
@@ -88,7 +74,22 @@ function App() {
               path="/writings"
               exact={true}
               component={() => (
-                <Suspense fallback={null}>
+                <Suspense
+                  fallback={
+                    <Flex
+                      flexDirection="row"
+                      mt={4}
+                      pt={5}
+                      justifyContent="center"
+                    >
+                      <H2 fontWeight={300} mr={2}>
+                        {" "}
+                        Loading{" "}
+                      </H2>
+                      <Loading type="spokes" color="currentcolor" />
+                    </Flex>
+                  }
+                >
                   <Writings />
                 </Suspense>
               )}
@@ -109,41 +110,34 @@ function App() {
                 </Suspense>
               )}
             ></Route>
-            <Route
-              path="/about"
-              exact={true}
-              component={() => (
-                <Suspense fallback={null}>
-                  <About />
-                </Suspense>
-              )}
-            />
+            <Route path="/about" exact={true} component={About} />
             <Route path="/writings" component={Home} />
             <Route
               path="/readings"
               exact={true}
               component={() => (
-                <Suspense fallback={null}>
+                <Suspense
+                  fallback={
+                    <Flex
+                      flexDirection="row"
+                      mt={4}
+                      pt={5}
+                      justifyContent="center"
+                    >
+                      <H2 fontWeight={300} mr={2}>
+                        {" "}
+                        Loading{" "}
+                      </H2>
+                      <Loading type="spokes" color="currentcolor" />
+                    </Flex>
+                  }
+                >
                   <Readings />
                 </Suspense>
               )}
             />
-            <Route
-              path="/frames"
-              exact={true}
-              component={() => (
-                <Suspense fallback={null}>
-                  <Frames />
-                </Suspense>
-              )}
-            />
-            <Route
-              component={() => (
-                <Suspense fallback={null}>
-                  <NotFound />
-                </Suspense>
-              )}
-            />
+            <Route path="/frames" exact={true} component={Frames} />
+            <Route component={NotFound} />
           </Switch>
         </Router>
       </Div>
