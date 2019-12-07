bdc05196ce9fe61548de2bc62a270ba7c4761d6b
syncatallah <ahmed.atallah@swvl.com>
2019-12-07 18:54:59 +0200

    try register again

--- src/pages/others/BornRich.js
+++ src/pages/others/BornRich.js
@@ -6,7 +6,6 @@ import { Div, H2 } from "../../elements";
 import Flex from "../../components/Flex";
 import Header from "../../containers/Header";
 import Loading from "../../components/Loading";
-import ErrorBoundary from "./error-boundary";
 const Content = lazy(() => import("!babel-loader!mdx-loader!./BornRich.mdx"));
 
 function BornRich({ ...props }) {
@@ -22,21 +21,19 @@ function BornRich({ ...props }) {
         fontFamily={theme.typefaces.helvetica}
         mt={40}
       >
-        <ErrorBoundary>
-          <Suspense
-            fallback={
-              <Flex flexDirection="row" justifyContent="center">
-                <H2 fontWeight={300} ml={2}>
-                  {" "}
-                  قيد التحميل{" "}
-                </H2>
-                <Loading type="spokes" color="currentcolor" />
-              </Flex>
-            }
-          >
-            <Content />
-          </Suspense>
-        </ErrorBoundary>
+        <Suspense
+          fallback={
+            <Flex flexDirection="row" justifyContent="center">
+              <H2 fontWeight={300} ml={2}>
+                {" "}
+                قيد التحميل{" "}
+              </H2>
+              <Loading type="spokes" color="currentcolor" />
+            </Flex>
+          }
+        >
+          <Content />
+        </Suspense>
       </Div>
     </>
   );
