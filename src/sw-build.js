ca5e9de8768bcc29d8154681e0ca6ed9652dbfed
syncatallah <ahmed.atallah@swvl.com>
2020-05-09 05:04:35 +0200

    precache all except images

--- src/sw-build.js
+++ src/sw-build.js
@@ -12,8 +12,7 @@ const buildSW = () => {
 
       globDirectory: 'build',
 
-      globPatterns: ['**/*.{js,css,html,jpg,jpeg,png,svg}'],
-      globIgnores: ['**/node_modules/**/*', 'static/media/*.{jpg,jpeg}'],
+      globPatterns: ['**/*.{js,css,html}'],
 
       maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
     })
