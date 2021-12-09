c3c2147350d160430e516d82263941f5975e7793
Ahmad Atallah <ahmed.atallah@swvl.com>
2021-12-09 02:19:34 +0200

    Add 2 archives (#70)

--- src/components/card.js
+++ src/components/card.js
@@ -7,14 +7,7 @@ import Image from 'gatsby-image';
 export const Cards = () => {
   const result = useStaticQuery(graphql`
     {
-      card1: file(name: { eq: "2021_0705_12202600" }) {
-        childImageSharp {
-          fluid(maxWidth: 1024) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-      card2: file(name: { eq: "DSCF8594" }) {
+      card1: file(name: { eq: "window-xp-palette-2" }) {
         childImageSharp {
           fluid(maxWidth: 1024) {
             ...GatsbyImageSharpFluid
@@ -22,14 +15,23 @@ export const Cards = () => {
         }
       }
 
-      card3: file(name: { eq: "DSCF8296" }) {
+      card2: file(name: { eq: "red - shadows on beach" }) {
         childImageSharp {
           fluid(maxWidth: 1024) {
             ...GatsbyImageSharpFluid
           }
         }
       }
-      card4: file(name: { eq: "bnw - bread maker" }) {
+
+      card3: file(name: { eq: "girl" }) {
+        childImageSharp {
+          fluid(maxWidth: 1024) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+
+      card4: file(name: { eq: "red room - black" }) {
         childImageSharp {
           fluid(maxWidth: 1024) {
             ...GatsbyImageSharpFluid
@@ -51,41 +53,11 @@ export const Cards = () => {
           variant: 'styles.navitem',
         }}
         as={Link}
-        to="/archive/burullus-island/"
+        to="/archive/window-xp-palette/"
       >
         <Card>
-          <Image fluid={image1} />
-          <h3>Burllus Island</h3>
-          <small sx={{ ml: 3, fontSize: '0.7rem', color: 'black' }}>
-            Archive
-          </small>
-        </Card>
-      </NavLink>
-      <NavLink
-        sx={{
-          variant: 'styles.navitem',
-        }}
-        as={Link}
-        to="/archive/burnt-trees/"
-      >
-        <Card>
-          <Image fluid={image2} />
-          <h3>Burnt Trees</h3>
-          <small sx={{ ml: 3, fontSize: '0.7rem', color: 'black' }}>
-            Archive
-          </small>
-        </Card>
-      </NavLink>
-      <NavLink
-        sx={{
-          variant: 'styles.navitem',
-        }}
-        as={Link}
-        to="/archive/the-office/"
-      >
-        <Card>
-          <Image fluid={image3} />
-          <h3>The Office</h3>
+          <Image style={{ height: '170px' }} fluid={image1} />
+          <h3>Windows XP, Generative Art</h3>
           <small sx={{ ml: 3, fontSize: '0.7rem', color: 'black' }}>
             Archive
           </small>
@@ -99,10 +71,40 @@ export const Cards = () => {
         to="/frames/"
       >
         <Card>
-          <Image fluid={image4} />
-          <h3>Bread Maker</h3>
+          <Image style={{ height: '170px' }} fluid={image2} />
+          <h3>When the Sun Hits</h3>
           <small sx={{ ml: 3, fontSize: '0.7rem', color: 'black' }}>
             Frames
+          </small>
+        </Card>
+      </NavLink>
+      <NavLink
+        sx={{
+          variant: 'styles.navitem',
+        }}
+        as={Link}
+        to="/archive/on-some-faraway-beach/"
+      >
+        <Card>
+          <Image style={{ height: '170px' }} fluid={image3} />
+          <h3>On Some Faraway Beach</h3>
+          <small sx={{ ml: 3, fontSize: '0.7rem', color: 'black' }}>
+            Archive
+          </small>
+        </Card>
+      </NavLink>
+      <NavLink
+        sx={{
+          variant: 'styles.navitem',
+        }}
+        as={Link}
+        to="/archive/red-room-generative-art/"
+      >
+        <Card>
+          <Image style={{ height: '170px' }} fluid={image4} />
+          <h3>Red Room, Generative Art</h3>
+          <small sx={{ ml: 3, fontSize: '0.7rem', color: 'black' }}>
+            Archive
           </small>
         </Card>
       </NavLink>
