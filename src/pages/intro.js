fb44c16d68e70ba01249b61f4940bdeb0e467e9f
ahmadatallah <me@atallahsan.cc>
2022-05-25 11:24:23 +0200

    Update the profile

--- src/pages/intro.js
+++ src/pages/intro.js
@@ -59,14 +59,14 @@ const TextShadow = (props) => {
               my: 5,
               marginTop: '20px',
               borderRadius: '10px',
-              boxShadow: `4px 4px 0px ${theme.colors.secondary}, 8px 8px 0px ${theme.colors.primary}, 12px 12px 0px ${theme.colors.background}, 16px 16px 0px ${theme.colors.background}, 20px 20px 0px ${theme.colors.primary}, 24px 24px 0px ${theme.colors.secondary}`,
+              boxShadow: `2px 2px 0px ${theme.colors.secondary}, 4px 4px 0px ${theme.colors.primary}, 8px 8px 0px ${theme.colors.background}, 10px 10px 0px ${theme.colors.background}, 12px 12px 0px ${theme.colors.primary}, 14px 14px 0px ${theme.colors.secondary}`,
             }}
           />
 
           <div
             {...props}
             sx={{
-              lineHeight: '2',
+              lineHeight: '1.5',
               letterSpacing: '0',
               color: 'hsl(0,0%,100%)',
               fontSize: [1, 2, 3],
@@ -77,100 +77,115 @@ const TextShadow = (props) => {
           >
             I'm Ahmad Atallah. A Software engineer at Aeditive. Based in
             Hamburg, Germany. Worked before at Swvl. Started the front-end team
-            to be now one of the region's biggest front-end teams. Currently I'm
-            more into Generative art/design and photography.
-          </div>
-        </div>
-        <div
-          {...props}
-          sx={{
-            lineHeight: '2',
-            letterSpacing: '0',
-            color: 'hsl(0,0%,100%)',
-            fontSize: [1, 2, 3],
-            mx: 'auto',
-            height: 'auto',
-          }}
-        >
-          Here, you can find more links about me:
-          <div
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-            }}
-          >
-            <a
-              href="https://twitter.com/ahmadatallah"
-              title="Twitter"
-              sx={{
-                variant: 'styles.navitem',
-              }}
-            >
-              <Twitter size={24} />
-            </a>
-            <a
-              href="mailto:hi@atallahsan.cc"
-              title="Email"
-              sx={{
-                variant: 'styles.navitem',
-                ml: 2,
-                mr: 2,
-              }}
-            >
-              <Email size={24} />
-            </a>
-            <a
-              href="https://github.com/ahmadatallah"
-              title="GitHub"
-              sx={{
-                variant: 'styles.navitem',
-                mr: 2,
-              }}
-            >
-              <GitHub size={24} />
-            </a>
-            <a
-              href="https://www.instagram.com/ahmad.atallah/"
-              title="Instagram"
-              sx={{
-                variant: 'styles.navitem',
-                mr: 2,
-              }}
-            >
-              <Instagram size={24} />
-            </a>
-            <a
-              href={result.pdf.publicURL}
-              title="Resume"
-              sx={{
-                variant: 'styles.navitem',
-                fontSize: '18px',
-                mr: 3,
-              }}
-            >
-              Resume
-            </a>
+            to be now one of the region's biggest teams. Currently, I'm more
+            into generative art/design and photography.
+            <br />
+            <br />I use this space to share some highlights from the daily
+            random stuff I do which you can find at the
             <NavLink
               as={Link}
               to="/archive"
               sx={{
                 variant: 'styles.navitem',
-                fontSize: '18px',
-                mr: 3,
+                fontSize: ['12px', '14px', '16px'],
+                mx: 1,
               }}
             >
               Archive
             </NavLink>
+            and sometimes drafts at
             <NavLink
               as={Link}
               to="/notes"
               sx={{
-                fontSize: '18px',
+                fontSize: ['12px', '14px', '16px'],
+                ml: 1,
                 variant: 'styles.navitem',
               }}
             >
               Notes
             </NavLink>
+            .
+            <div
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                flexWrap: 'wrap',
+              }}
+            >
+              I'm also privately active on
+              <a
+                href="https://twitter.com/ahmadatallah"
+                title="Twitter"
+                sx={{
+                  variant: 'styles.navitem',
+                  mx: 2,
+                }}
+              >
+                <Twitter size={24} />
+              </a>
+              <a
+                href="https://github.com/ahmadatallah"
+                title="GitHub"
+                sx={{
+                  variant: 'styles.navitem',
+                  mr: 2,
+                }}
+              >
+                <GitHub size={20} />
+              </a>
+              <a
+                href="https://www.instagram.com/ahmad.atallah/"
+                title="Instagram"
+                sx={{
+                  variant: 'styles.navitem',
+                  mr: 2,
+                }}
+              >
+                <Instagram size={20} />
+              </a>
+              .
+            </div>
+            <div
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                flexWrap: 'wrap',
+              }}
+            >
+              You can contact me via E-mail at{' '}
+              <a
+                href="mailto:hi@atallahsan.cc"
+                title="Email"
+                sx={{
+                  variant: 'styles.navitem',
+                  fontSize: ['12px', '14px', '16px'],
+                  ml: [0, 1, 1],
+                }}
+              >
+                me@atallahsan.cc
+              </a>
+            </div>
+            <div
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                flexWrap: 'wrap',
+              }}
+            >
+              The last of the unimportance you can do here is checking my
+              <a
+                href={result.pdf.publicURL}
+                title="Resume"
+                sx={{
+                  variant: 'styles.navitem',
+                  fontSize: ['12px', '14px', '16px'],
+                  ml: 1,
+                }}
+              >
+                Resume
+              </a>
+            </div>
           </div>
         </div>
       </div>
